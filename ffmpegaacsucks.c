cb39dede2865f1f8365ed3203117efd839214f74
odrling <florianbadie@odrling.xyz>
2023-12-03 02:26:47 +0100

    fix results on file with several AAC tracks
    fail when any track is a lavc aac

--- ffmpegaacsucks.c
+++ ffmpegaacsucks.c
@@ -6,17 +6,23 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define NOT_FOUND_STR "no Lavc/FFmpeg AAC stream was found\n"
+
+#define FFMPEGAACSUCKS_DETECTED 1
+#define FFMPEGAACSUCKS_UNDETECTED 0
+#define FFMPEGAACSUCKS_FAILURE -1
+
 
 int ffaacsucks_check(char* filepath) {
     AVFormatContext *s = NULL;
     unsigned int i, aac_streams;
-    int ret, return_value = EXIT_SUCCESS;
+    int ret, return_value = FFMPEGAACSUCKS_UNDETECTED;
 
     ret = avformat_open_input(&s, filepath, NULL, NULL);
 
     if (ret < 0) {
         perror("failed to load file");
-        exit(EXIT_FAILURE);
+        exit(FFMPEGAACSUCKS_FAILURE);
     }
     aac_streams = 0;
 
@@ -31,11 +37,8 @@ int ffaacsucks_check(char* filepath) {
         }
     }
 
-    #define NOT_FOUND_STR "no Lavc/FFmpeg AAC stream was found\n"
-
     if (aac_streams == 0) {
         printf(NOT_FOUND_STR);
-        return_value = EXIT_FAILURE;
         goto free_avf;
     }
 
@@ -52,9 +55,9 @@ int ffaacsucks_check(char* filepath) {
         aac_streams--;
         if (cmp == 0) {
             printf("stream %d is a Lavc/FFmpeg AAC stream\n", pkt->stream_index);
+            return_value = FFMPEGAACSUCKS_DETECTED;
         } else if (aac_streams == 0) {
             printf(NOT_FOUND_STR);
-            return_value = EXIT_FAILURE;
         }
 
         av_packet_unref(pkt);
@@ -71,8 +74,13 @@ free_avf:
 
 int main(int argc, char *argv[])
 {
-    if (argc < 2)
+    if (argc < 2) {
+        fprintf(stderr, "usage: ffmpegaacsucks <file>");
         return EXIT_FAILURE;
+    }
 
-    return ffaacsucks_check(argv[1]);
+    if (ffaacsucks_check(argv[1]) == FFMPEGAACSUCKS_DETECTED)
+        return EXIT_SUCCESS;
+    else
+        return EXIT_FAILURE;
 }
