21dba74282f88af43ce428119a02a0ec6242ba3a
odrling <florianbadie@odrling.xyz>
2023-12-06 00:15:34 +0100

    make ffaacsucks_check_aac_stream_packet public

--- ffmpegaacsucks.h
+++ ffmpegaacsucks.h
@@ -2,6 +2,7 @@
 #define FFMPEGAACSUCKS_H 1
 
 #include <libavformat/avformat.h>
+#include <stdbool.h>
 
 #define FFMPEGAACSUCKS_DETECTED 1
 #define FFMPEGAACSUCKS_UNDETECTED 0
@@ -17,6 +18,8 @@ struct ffaacsucks_result *ffaacsucks_check(char *filepath);
 // NOTE: ffaacsucks_check_avfcontext will read frames from aac stream and discard other streams
 // which would work poorly with other uses of the avf context
 struct ffaacsucks_result *ffaacsucks_check_avfcontext(AVFormatContext *s, char *filepath);
+
+bool ffaacsucks_check_aac_stream_packet(AVPacket *pkt, char *filepath);
 
 void ffaacsucks_result_free(struct ffaacsucks_result *res);
 
