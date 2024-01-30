125468f8150296000c03772d91f46ee1cc90f5d1
odrling <florianbadie@odrling.xyz>
2024-01-30 19:43:10 +0100

    ci: less log

--- ci/build.sh
+++ ci/build.sh
@@ -7,7 +7,6 @@ if [ "$ARCH" != x86_64 ]; then
 fi
 
 meson setup build --buildtype release --strip -Dffmpeg:programs=disabled -Dffmpeg:tests=disabled -Dffmpeg:encoders=disabled -Dffmpeg:muxers=disabled -Dffmpeg:avfilter=disabled -Dffmpeg:avdevice=disabled -Dffmpeg:postproc=disabled -Dffmpeg:swresample=disabled -Dffmpeg:swscale=disabled -Dffmpeg:decoders=disabled -Dffmpeg:aac_decoder=enabled -Dffmpeg:aac_fixed_decoder=enabled -Dffmpeg:aac_latm_decoder=enabled -Dffmpeg:version3=enabled $extra_args
-cat /ffmpegaacsucks/build/meson-logs/meson-log.txt
 
 meson compile -C build
 meson install -C build --destdir=../dest/
